b48c7d42481cc0c101ca8629ffbdfaa6306215b4
auroraug <104081545+auroraug@users.noreply.github.com>
2023-12-06 22:42:43 +0800

    Update ClaimZkpepe.js

--- ClaimZkpepe.js
+++ ClaimZkpepe.js
@@ -4,40 +4,46 @@ const privateKey = [/**private key array */]
 const Wallet = privateKey.map(p => new ethers.Wallet(p,provider))
 
 async function main() {
-    const transactions = await Promise.all(Wallet.map(wallet => createTx(wallet)))
-    if(transactions.length == Wallet.length) {
-        for (let index = 0; index < Wallet.length; index++) {
-            const tx = await Wallet[index].sendTransaction(transactions[index]);
-            await tx.wait()
-            await sleep(1000) // 1s
-            console.log('TxHash:',tx.hash)
-        }
+    for (let index = 0; index < Wallet.length; index++) {
+        await claim(Wallet[index])
+        await sleep(1000)
     }
 }
 main()
 
-async function createTx(wallet) {
+async function claim(wallet) {
     try {
         const [amount] = await fetch(`https://www.zksyncpepe.com/resources/amounts/${wallet.toLowerCase()}.json`, {
             method: 'GET'
         })
         .then(async(response) => {
-            return await response.json()
+            return response.json()
+        }).catch(error => {
+            console.error('地址不符合：',Wallet[0])
+            return 'f';
         })
-        const proof = await fetch(`https://www.zksyncpepe.com/resources/proofs/${wallet.toLowerCase()}.json`, {
-            method: 'GET'
-        }).then(async(response) => {
-            return await response.json()
-        })
-        const calldata = '0x3b439351'+ethers.AbiCoder.defaultAbiCoder().encode(['bytes32[]','uint256'],[proof,ethers.parseUnits(amount.toString(),18)]).substring(2)
-        const tx = {
-            to: '0x95702a335e3349d197036Acb04BECA1b4997A91a',
-            value: 0,
-            gasPrice: ethers.parseUnits('0.25','gwei'),
-            gasLimit: 500000,
-            data: calldata
+        if (amount !== 'f') {
+            // console.log(amount)
+            const proof = await fetch(`https://www.zksyncpepe.com/resources/proofs/${wallet.toLowerCase()}.json`, {
+                method: 'GET'
+            }).then(async(response) => {
+                return await response.json()
+            })
+
+            const calldata = '0x3b439351'+ethers.AbiCoder.defaultAbiCoder().encode(['bytes32[]','uint256'],[proof,ethers.parseUnits(amount.toString(),18)]).substring(2)
+            const tx = {
+                to: '0x95702a335e3349d197036Acb04BECA1b4997A91a',
+                value: 0,
+                gasPrice: ethers.parseUnits('0.25','gwei'),
+                gasLimit: 500000,
+                data: calldata
+            }
+            const result = await wallet.sendTransaction(tx)
+            await result.wait()
+            console.log(`${wallet.address} 完成领取，数量${amount}`)
+        }else {
+            console.log(`${wallet.address} 该地址不满足要求`)
         }
-        return tx;
     } catch (error) {
         console.log(error.message)
     }
